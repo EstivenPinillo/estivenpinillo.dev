2ab416c41c3bdfcb181000e0ff9c688a0f8946ee
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-02-16 17:48:52 -0500

    feat: change the skills

---
+++ index.php
@@ -0,0 +1,375 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estiven Pinillo</title>
+    <link rel="shortcut icon" href="profile.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/project.css">
+</head>
+<body>
+    
+
+    <main class="main-grid">
+
+        <nav class="navigation font-federo">
+            <ul class="navigation-opt">
+                <!--
+                <li id="devsoftware" class="opt fs-m color-1e1b18">
+                    <span class="opt-software"></span>
+                    <span id="devsoftware-opt">Solicitar D. Software</span>
+                    <span class="opt-software"></span>
+                </li>-->
+                <a class="color-1e1b18" href="#developed-software"><li class="opt fs-m">Proyectos</li></a>
+                <a class="color-1e1b18" href="#titulacion"><li class="opt fs-m">Estudios</li></a>
+            </ul>
+        </nav>
+
+        <header class="header-main mt-60">
+            <h1 class="font-federo fs-b color-280659">
+                <span id="marlon">Marlon</span> Estiven Pinillo <span id="carabali">Carabali</span>
+            </h1>
+            <div>
+                <span class="font-marcellus">Tecnologo en analisis y desarrollo de software</span>
+                <span class="font-federo ml-10 color-5632a8">SENA</span>
+            </div>
+            <div>
+                <span class="font-marcellus">Tecnico en programacion de software.</span>
+            </div>
+        </header>
+
+        <section class="gridcol-3 mt-40">
+            <div class="inf-tecnology__profile">
+                <img class="inf-tecnology__profile__img" src="profile.png" alt="Retrato Abstracto">
+            </div>
+        </section>
+
+        <section class="gridcol-3 mt-40">
+                <header>
+                    <h2 class="fs-m font-federo color-280659">
+                        Habilidades de analisis y desarrollo de software - ADSO
+                    </h2>
+                </header>
+                <ul class="conocimiento font-marcellus mt-40">
+                    <li>PHP</li>
+                    <li>Laravel</li>
+                    <li>Angular</li>
+                    <li>JavaScript</li>
+                    <li>TypeScript</li>
+                    <li>Composer</li>
+                    <li>Flight</li>
+                    <li>MySQL</li>
+                    <li>MongoDB</li>
+                    <li>SQLite</li>
+                    <li>SQL</li>
+                    <li>API (RESTful, SOAP y WebSockets)</li>
+                    <li>Principios SOLID</li>
+                    <li>Estructuras de datos</li>
+                    <li>Algoritmos</li>
+                    <li>POO - Programacion orientada a objetos</li>
+                    <li>Patrones de diseño de software</li>
+                    <li>Arquitectura de software (MVC)</li>
+                    <li>Nginx</li>
+                    <li>Git</li>
+                    <li>GitHub</li>
+                    <li>GitFlow</li>
+                    <li>JQuery</li>
+                    <li>D3.js</li>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                    <li>SASS</li>
+                    <li>Metodologia BEM</li>
+                    <li>Metodologia OOCSS</li>
+                    <li>Responsive design</li>
+                    <li>Mobile first design</li>
+                    <li>Pruebas de software</li>
+                    <li>Scrum</li>
+                    <li>UML</li>
+                    <li>Linux</li>
+                    <li>C++</li>
+                    <li>Google Cloud</li>
+                    <li>Obtencion, analisis y documentacion de requerimientos</li>
+                </ul>
+        </section>
+
+        <section id="developed-software" class="developed-software mt-60">
+            <header class="header-section t-center">
+                <a class="font-federo fs-b color-280659" href="#developed-software">Software <span>Desarrollado</span></a>
+            </header>
+            <section class="software-list mt-40">
+                
+                <article class="software t-center font-philosopher br-4">
+                    <h2 class="header-sub t-center fs-m font-federo ">Jardineria</h2>
+                    <div class="img-soft br-4">
+                        <a href="jardineria/index.html" target="_blank"><img class="img-soft object-fit-cover br-4" src="jardineria/img/screempage.png" alt=""></a>
+                        <div class="developed-in">
+                            <span class="technology">HTML5</span>
+                            <span class="technology">CSS3</span>
+                        </div>
+                    </div>
+                </article>
+                
+                <article class="software t-center font-philosopher br-4">
+                    <h2 class="header-sub t-center fs-m font-federo ">AdmExpense</h2>
+                    <div class="img-soft br-4 ">
+                        <a href="https://github.com/EstivenPinillo/AdmExpense/tree/dev" target="_blank"><img class="img-soft object-fit-cover br-4" src="AdmExpense/AdmExpense.png" alt=""></a>
+                        <div class="developed-in">
+                            <span class="technology ">PHP</span>
+                            <span class="technology ">JS</span>
+                            <span class="technology ">HTML5</span>
+                            <span class="technology ">CSS3</span>
+                            
+                        </div>
+                    </div>
+                </article>
+
+                <article class="software t-center font-philosopher br-4">
+                    <h2 class="header-sub t-center fs-m font-federo ">Devchallenges My team page</h2>
+                    <div class="img-soft br-4 ">
+                        <a href="myteampage/index.html" target="_blank"><img class="img-soft object-fit-cover br-4" src="myteampage/img/teampage.png" alt=""></a>
+                        <div class="developed-in">
+                            <span class="technology ">HTML5</span>
+                            <span class="technology ">CSS3</span>
+                        </div>
+                    </div>
+                </article>
+
+                <article class="software t-center font-philosopher br-4">
+                    <h2 class="header-sub t-center fs-m font-federo ">Devchallenges 404 Not found</h2>
+                    <div class="img-soft br-4 ">
+                        <a href="notfound404/index.html" target="_blank"><img class="img-soft object-fit-cover br-4" src="notfound404/img/notfound.png" alt=""></a>
+                        <div class="developed-in">
+                            <span class="technology ">HTML5</span>
+                            <span class="technology ">CSS3</span>
+                        </div>
+                    </div>
+                </article>
+                
+            </section>
+        </section>
+
+        <section class="developed-software mt-40">
+            <header class="header-section t-center">
+                <h2 class="font-federo fs-b color-280659 mt-40">Certificados</h2>
+            </header>
+            <section class="cetificado-list mt-40 font-marcellus">
+
+                <article class="certificado t-center">
+                    <div class="fs-m header-sub">UDEMY</div>
+                    <div class="border-D8315B">
+                        <img class="width-100" src="certificados/UdemyPHP.jpg" alt="">
+                    </div>
+                    <div class="font-federo fs-m header-sub">Curso completo de PHP</div>
+                </article>
+
+                <article class="certificado t-center">
+                        <div class="fs-m header-sub">Microsoft + GitHub</div>
+                        <div class="border-D8315B">
+                            <img class="width-100" src="certificados/GitHubUniverse.png" alt="">
+                        </div>
+                    <div class="font-federo fs-m header-sub">GitHub Universe 2023 Cloud Skills Challenge</div>
+                </article>
+
+                <article class="certificado t-center">
+                        <div class="fs-m header-sub">SENA</div>
+                        <div class="border-D8315B">
+                            <img class="width-100" src="certificados/MySQL.png" alt="">
+                        </div>
+                    <div class="font-federo fs-m header-sub">Construccion de base de datos con MySQL</div>
+                </article>
+
+                <article class="certificado t-center">
+                        <div class="fs-m header-sub">SENA</div>
+                        <div class="border-D8315B">
+                            <img class="width-100" src="certificados/disenodecasosdeuso.png" alt="">
+                        </div>
+                    <div class="font-federo fs-m header-sub">Diseño de casos de uso</div>
+                </article>
+
+                <article class="certificado t-center">
+                        <div class="fs-m header-sub">SENA</div>
+                        <div class="border-D8315B">
+                            <img class="width-100" src="certificados/variablesyestructutasJava.png" alt="">
+                        </div>
+                    <div class="font-federo fs-m header-sub">
+                        Variables y estructuras de control en la programacion orientada a objectos: JAVA
+                    </div>
+                </article>
+
+                <article class="certificado t-center">
+                        <div class="fs-m header-sub">SENA</div>
+                        <div class="border-D8315B">
+                            <img class="width-100" src="certificados/algoritmia.png" alt="">
+                        </div>
+                    <div class="font-federo fs-m header-sub">Principios de algoritmia</div>
+                </article>
+
+                <article class="certificado t-center">
+                        <div class="fs-m header-sub">SENA</div>
+                        <div class="border-D8315B">
+                            <img class="width-100" src="certificados/Unity.png" alt="">
+                        </div>
+                    <div class="font-federo fs-m header-sub">Diseño de videojuegos en tercera persona con Unity</div>
+                </article>
+
+
+                <article class="certificado t-center">
+                        <div class="fs-m header-sub">Universidad Católica de Chile</div>
+                        <div class="border-D8315B">
+                            <img class="width-100" src="certificados/Python.png" alt="">
+                        </div>
+                    <div class="font-federo fs-m header-sub">
+                        Introducción a la programación en Python I: Aprendiendo a programar con Python
+                    </div>
+                </article>
+
+                <article class="certificado t-center">
+                        <div class="fs-m header-sub">Deep Teaching Solutions</div>
+                        <div class="border-D8315B">
+                            <img class="width-100" src="certificados/DeepLearning.png" alt="">
+                        </div>
+                    <div class="font-federo fs-m header-sub">
+                        Aprendiendo a aprender: Poderosas herramientas mentales con las que podrás dominar temas difíciles (Learning How to Learn)
+                    </div>
+                </article>
+
+                <article class="certificado t-center">
+                        <div class="fs-m header-sub">Domestika</div>
+                        <div class="border-D8315B">
+                            <img class="width-100" src="certificados/GridFlexbox.png" alt="">
+                        </div>
+                    <div class="font-federo fs-m header-sub">
+                        Layout web con CSS Grid, Flexbox y otras técnicas modernas
+                    </div>
+                </article>
+
+                <article class="certificado t-center">
+                        <div class="fs-m header-sub">Domestika</div>
+                        <div class="border-D8315B">
+                            <img class="width-100" src="certificados/InterfacesWeb.png" alt="">
+                        </div>
+                    <div class="font-federo fs-m header-sub">
+                        Diseño de interfaces para sitios web y aplicaciones
+                    </div>
+                </article>
+
+                <article class="certificado t-center">
+                        <div class="fs-m header-sub">Domestika</div>
+                        <div class="border-D8315B">
+                            <img class="width-100" src="certificados/HTML5CSS3.png" alt="">
+                        </div>
+                    <div class="font-federo fs-m header-sub">
+                        Técnicas de desarrollo web con HTML5 y CSS3
+                    </div>
+                </article>
+
+                <article class="certificado t-center">
+                        <div class="fs-m header-sub">Domestika</div>
+                        <div class="border-D8315B">
+                            <img class="width-100" src="certificados/JavaScript.png" alt="">
+                        </div>
+                    <div class="font-federo fs-m header-sub">
+                        Introducción a la programación en JavaScript
+                    </div>
+                </article>
+
+                <article class="certificado t-center">
+                        <div class="fs-m header-sub">Domestika</div>
+                        <div class="border-D8315B">
+                            <img class="width-100" src="certificados/WebResponsive.png" alt="">
+                        </div>
+                    <div class="font-federo fs-m header-sub">
+                        Introducción al Desarrollo Web Responsive con HTML y CSS
+                    </div>
+                </article>
+
+                <article class="certificado t-center">
+                        <div class="fs-m header-sub">Codigo Facilito</div>
+                        <div class="border-D8315B">
+                            <img class="width-100" src="certificados/PHPCodigoFacilito.png" alt="">
+                        </div>
+                    <div class="font-federo fs-m header-sub">
+                        Curso profesional de PHP
+                    </div>
+                </article>
+
+                <article class="certificado t-center">
+                        <div class="fs-m header-sub">Codigo Facilito</div>
+                        <div class="border-D8315B">
+                            <img class="width-100" src="certificados/DesarrolloWeb.png" alt="">
+                        </div>
+                    <div class="font-federo fs-m header-sub">
+                        Curso profesional de Desarrollo Web
+                    </div>
+                </article>
+
+                <article class="certificado t-center">
+                        <div class="fs-m header-sub">Codigo Facilito</div>
+                        <div class="border-D8315B">
+                            <img class="width-100" src="certificados/IntroduccionProgramacion.png" alt="">
+                        </div>
+                    <div class="font-federo fs-m header-sub">
+                        Curso de introducción a la programación
+                    </div>
+                </article>
+
+                <article class="certificado t-center">
+                        <div class="fs-m header-sub">Codigo Facilito</div>
+                        <div class="border-D8315B">
+                            <img class="width-100" src="certificados/ProfesionalJava.png" alt="">
+                        </div>
+                    <div class="font-federo fs-m header-sub">
+                        Curso profesional de JAVA
+                    </div>
+                </article>
+
+            </section>
+        </section>
+        
+        <section class="gridcol-3 mt-60" id="titulacion">
+            <header class="t-center">
+                <a class="color-280659" href="#titulacion"><h2 class="font-federo fs-b">Titulación</h2></a>
+            </header>
+            <section class="titulacion jc-spacearound mt-40">
+                <!--<img class="tit-img" src="klipartz.com.png" alt="">-->
+                <div class="titulo t-center font-federo fs-m">
+                    <div class="tit-img">Tecnico En Programacion De Software</div>
+                    <img class="tit-img " src="tecnicoprogramacion.png" alt="">
+                </div>
+                <!--<img class="tit-img tit-reverse" src="klipartz.com.png" alt="">-->
+                <div class="titulo t-center font-federo fs-m">
+                    <div class="tit-img">Tecnologo En Analisis Y Desarrollado De Software</div>
+                    <div class="soon-to-be-certified">
+                        <img class="tit-img  titulo-next" src="tecnicoprogramacion.png" alt="">
+                        <p id="certified" class="color-5632a8">En Certificación . . .</p>
+                    </div>
+                </div>
+            </section>
+        </section>
+
+        <section class="gridcol-3 mt-40 display-none">
+            <header>
+                <h2 class="font-federo fs-b ">Repositorio <span class="">/</span> Contacto</h2>
+            </header>
+            <div class="mt-40 fs-m fw-bold mb-30">
+                <div class="header-sub">GitHub</div>
+                <a class="ml-10" href="https://github.com/EstivenPinillo?tab=overview&from=2024-04-01&to=2024-04-22" target="_blank">Marlon Estiven Pinillo Carabali</a>
+            </div>
+            <div class="fs-m fw-bold">
+                <div class="header-sub">Email</div>
+                <div class="ml-10">estivenpinillo.software@hotmail.com</div>
+            </div>
+        </section>
+        
+        <footer class="foot fw-bold font-marcellus">
+            © Copyright 2023 - 2024
+        </footer> 
+
+    </main>
+
+
+</body>
+</html>
