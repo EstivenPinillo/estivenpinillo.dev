715be739b0714d2b08b637fefc46853343b66518
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-04-01 16:49:36 -0500

    feat: add perfil component

---
+++ src/app/features/perfil/perfil.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { EstivenComponent } from '../estiven/estiven.component';
+
+@Component({
+  selector: 'app-perfil',
+  imports: [EstivenComponent],
+  templateUrl: './perfil.component.html',
+  styleUrl: './perfil.component.scss'
+})
+export class PerfilComponent {
+
+    public estiven:string = "./assets/img/estiven.jpg";
+}
