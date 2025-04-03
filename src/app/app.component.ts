a8b3f117a3ffe450696841b618e974557620a053
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-04-02 23:57:43 -0500

    feat: changes for the software component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { PerfilComponent } from './features/perfil/perfil.component';
-import { EstivenComponent } from './features/estiven/estiven.component';
-import { SkillsComponent } from './features/skills/skills.component';
+//import { EstivenComponent } from './features/estiven/estiven.component';
+//import { SkillsComponent } from './features/skills/skills.component';
 import { NavbarComponent } from './features/navbar/navbar.component';
 import { WaveComponent } from './features/wave/wave.component';
 import { SoftwareComponent } from './features/software/software.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, PerfilComponent, EstivenComponent,
-            SkillsComponent, WaveComponent, SoftwareComponent],
+  imports: [NavbarComponent, PerfilComponent, SoftwareComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
