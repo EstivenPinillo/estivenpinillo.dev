1280726653f562ddd83e4408a050fcc12fdf4457
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-04-02 23:50:13 -0500

    feat: add software component

---
+++ src/app/features/software/software.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ProjectComponent } from '../project/project.component';
+
+@Component({
+  selector: 'app-software',
+  imports: [ProjectComponent],
+  templateUrl: './software.component.html',
+  styleUrl: './software.component.scss'
+})
+export class SoftwareComponent {
+
+}
