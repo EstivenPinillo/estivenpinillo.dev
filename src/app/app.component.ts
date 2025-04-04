c26ab43cfcabc3dc7da6dde2eed3d88ed5ebf380
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-04-04 15:33:59 -0500

    feat: add certificate component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { PerfilComponent } from './features/perfil/perfil.component';
-//import { EstivenComponent } from './features/estiven/estiven.component';
 import { SkillsComponent } from './features/skills/skills.component';
 import { NavbarComponent } from './features/navbar/navbar.component';
-//import { WaveComponent } from './features/wave/wave.component';
 import { SoftwareComponent } from './features/software/software.component';
 import { ExperienceComponent } from "./features/experience/experience.component";
+import { CertificateComponent } from './features/certificate/certificate.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, PerfilComponent, SoftwareComponent, ExperienceComponent, SkillsComponent],
+  imports: [NavbarComponent, PerfilComponent, SoftwareComponent, ExperienceComponent, SkillsComponent, CertificateComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
