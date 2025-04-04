c593001cb87d3d27550dc402e8c6cabb160309cf
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-04-03 22:20:44 -0500

    feat: changes needed for the experience component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { PerfilComponent } from './features/perfil/perfil.component';
 //import { EstivenComponent } from './features/estiven/estiven.component';
 //import { SkillsComponent } from './features/skills/skills.component';
 import { NavbarComponent } from './features/navbar/navbar.component';
-import { WaveComponent } from './features/wave/wave.component';
+//import { WaveComponent } from './features/wave/wave.component';
 import { SoftwareComponent } from './features/software/software.component';
+import { ExperienceComponent } from "./features/experience/experience.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, PerfilComponent, SoftwareComponent],
+  imports: [NavbarComponent, PerfilComponent, SoftwareComponent, ExperienceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
