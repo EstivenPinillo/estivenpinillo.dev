b571217313bf9dd54342c6ba607aa28846bd1395
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-04-04 15:20:28 -0500

    feat: add resizing to grid-column

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { PerfilComponent } from './features/perfil/perfil.component';
 //import { EstivenComponent } from './features/estiven/estiven.component';
-//import { SkillsComponent } from './features/skills/skills.component';
+import { SkillsComponent } from './features/skills/skills.component';
 import { NavbarComponent } from './features/navbar/navbar.component';
 //import { WaveComponent } from './features/wave/wave.component';
 import { SoftwareComponent } from './features/software/software.component';
@@ -9,7 +9,7 @@ import { ExperienceComponent } from "./features/experience/experience.component"
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, PerfilComponent, SoftwareComponent, ExperienceComponent],
+  imports: [NavbarComponent, PerfilComponent, SoftwareComponent, ExperienceComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
