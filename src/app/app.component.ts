2c8294850e6285aae35db125014a5186dfc9ede9
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-04-17 20:35:22 -0500

    feat: add header to who-am-i component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,11 @@ import { NavbarComponent } from './features/navbar/navbar.component';
 import { SoftwareComponent } from './features/software/software.component';
 import { ExperienceComponent } from "./features/experience/experience.component";
 import { CertificateComponent } from './features/certificate/certificate.component';
+import { WhoAmIComponent } from './features/who-am-i/who-am-i.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, PerfilComponent, SoftwareComponent, ExperienceComponent, SkillsComponent, CertificateComponent],
+  imports: [NavbarComponent, PerfilComponent, SoftwareComponent, ExperienceComponent, SkillsComponent, CertificateComponent, WhoAmIComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
