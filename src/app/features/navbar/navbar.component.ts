08761301e0ea6007b1ad9a07565d9e1116fbe97c
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-04-18 22:34:10 -0500

    feat: change links in navbar

--- src/app/features/navbar/navbar.component.ts
+++ src/app/features/navbar/navbar.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
 
+interface MenuOption {
+    href: string;
+    name: string;
+}
+
 @Component({
-  selector: 'app-navbar',
-  imports: [],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+    selector: 'app-navbar',
+    imports: [],
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
+    menuOptions: MenuOption[] = [
+
+        {
+            'href': '#perfil',
+            'name': 'Perfil'
+        },
+        {
+            'href': '#habilidades',
+            'name': 'Habilidades'
+        },
+        {
+            'href': '#proyectos',
+            'name': 'Proyectos'
+        },
+        {
+            'href': '#experiencia',
+            'name': 'Experiencia'
+        },
+        {
+            'href': '#estudios',
+            'name': 'Estudios'
+        }
+    ];
 }
