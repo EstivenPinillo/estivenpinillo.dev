440d791d21939efbdf7179853924aa6ef6a19c1c
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-04-25 01:52:07 -0500

    fix: add attributes of the profile component

--- src/app/features/perfil/perfil.component.ts
+++ src/app/features/perfil/perfil.component.ts
@@ -9,6 +9,15 @@ import { EstivenComponent } from '../estiven/estiven.component';
 })
 export class PerfilComponent {
 
-    ephemeralIntroduction: string = "";
+    firtName: string = "Marlon";
+    lastName: string = "Carabali";
+    name: string = "Estiven Pinillo";
+    ephemeralProfile: string = "Perfil efímero";
+    softwareDevelopment: string = "Tecnólogo en análisis y desarrollo de software";
+    developmentFullStack: string = "Desarrollador web full-stack";
     
+    descriptionProfile: string =   `Gozo de una sólida formación técnica y experiencia en la construcción de soluciones web eficientes.
+                                    Me especializo en Laravel para el backend y Angular.js para el frontend, integro ambas tecnologías para desarrollar aplicaciones robustas y escalables.
+                                    Aplico los principios de ingeniería de requerimientos y estándares de calidad basados en la norma ISO/IEC 25000, 
+                                    asegurando desarrollos alineados a las necesidades del usuario y con altos niveles de calidad.`;
 }
