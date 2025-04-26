e714613be64ad46e4614abf6f82821f8e4db6a44
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-04-25 21:11:45 -0500

    feat: refactor skills component, add font klik, remove fonts

--- src/app/features/skills/skills.component.ts
+++ src/app/features/skills/skills.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-skills',
-  imports: [],
-  templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+    selector: 'app-skills',
+    imports: [],
+    templateUrl: './skills.component.html',
+    styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
+
+    programmingLanguages: string[] = ["PHP","TypeScript","JavaScript", "C++"];
+    frameworksLibraries: string[] = ["Laravel", "Angular", "Flight", "JQuery", "D3.js"];
+    databases: string[] = ["MySQL", "MongoDB", "SQLite", "SQL"];
+    principlesParadigms: string[] = ["POO", "SOLID", "Patrones de diseño", "Arquitectura software","Estructuras de datos"];
+    designLayout: string[] = ["HTML5", "CSS3", "SASS", "BEM", "OOCSS", "Responsive design", "mobile-first", "UML"];
+    servicesApis: string[] = ["RESTful APIs", "SOAP", "WebSockets"];
+    testing: string[] = ["Pruebas de software", "PHPUnit"];
+    methodologies: string[] = ["Scrum"];
+    projectDependencies: string[] = ["Composer"];
+    quality: string[] = ["ISO/IEC 25000"];
+    devOpsTools: string[] = ["Google Cloud", "Git", "GitHub", "GitFlow", "Nginx", "Linux"];
+    analysis: string[] = ["Ingeniería de requerimientos"];
+
+    skills: string[] = [
+        ...this.programmingLanguages, 
+        ...this.frameworksLibraries, 
+        ...this.databases,
+        ...this.principlesParadigms,
+        ...this.designLayout,
+        ...this.servicesApis,
+        ...this.testing,
+        ...this.methodologies,
+        ...this.projectDependencies,
+        ...this.quality,
+        ...this.devOpsTools,
+        ...this.analysis
+    ];
 
 }
